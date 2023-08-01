540aa4b18282295eedde45f45076a0785384839d
Victor Tr <victor.tran@hubble.sg>
2023-08-01 09:22:01 +0700

    - Add Graph

--- src/DataStructure/NonLinear/Graph/Graph.java
+++ src/DataStructure/NonLinear/Graph/Graph.java
@@ -1,12 +1,13 @@
 package DataStructure.NonLinear.Graph;
 
 import java.util.LinkedList;
+import java.util.List;
 
-public class Graph {
+public class Graph<E> {
 
     int vertices;
 
-    LinkedList<LinkedList<Integer>> adjacencyList;
+    LinkedList<LinkedList<E>> adjacencyList;
 
     public static void main(String[] args) {
         //
@@ -14,10 +15,23 @@ public class Graph {
 
     public Graph(int vertices) {
         this.vertices = vertices;
-        this.adjacencyList = new LinkedList<>();
+        this.adjacencyList = new LinkedList<LinkedList<E>>();
 
         for (int i = 0; i < vertices; i++) {
-            adjacencyList.add(new LinkedList<>());
+            adjacencyList.add(new LinkedList<E>());
         }
+
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        list.add(10);
+        Integer val = list.get(0);
+    }
+
+    public void addEdge(int fromVertexIndex, int toVertexIndex) {
+        this.adjacencyList.get(fromVertexIndex).add(toVertexIndex);
+        this.adjacencyList.get(toVertexIndex).add(fromVertexIndex);
+    }
+
+    public void removeEdge(int fromVertex, int toVertex) {
+
     }
 }
